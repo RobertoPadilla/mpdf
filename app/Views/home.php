b70198131d4f0819d430b364d8b745a904589514
Roberto Ivan <basshivan981@gmail.com>
2020-04-18 14:52:22 -0500

    Se añadieron cambios simples para comenzar
    Boostrap, SASS, se modificó el archivo gitignore y la vista principal.

---
+++ app/Views/home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Mpdf library test</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+	<h1 class="text-center text-white mt-3">Here we can make a PDF file</h1>
+</body>
+</html>
