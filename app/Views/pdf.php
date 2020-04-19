e30100ea9e13968150e3965d98edb0035c244fc1
Roberto Ivan <basshivan981@gmail.com>
2020-04-18 19:17:47 -0500

    Creada la vista en HTML del documento
    Ahora solo falta crear los headers y footer a traves de la libreria y
    convertir el HTML a pdf.

--- app/Views/pdf.php
+++ app/Views/pdf.php
@@ -7,6 +7,65 @@
 	<link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-  
+  <div class="row" style="max-width: 100%">
+    <div class="col"></div>
+    <div  id="pdf-body" class="col-8 bg-white">
+      <div class="pdf-file text-justify">
+        <div class="page1 my-5 mx-3">
+          <h2 class="text-center mb-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, molestiae. Blanditiis beatae adipisci soluta similique incidunt doloremque in minima, sunt itaque ex aspernatur nesciunt aut expedita saepe, dignissimos animi laboriosam!
+            A saepe itaque dicta fugiat repellendus error laborum obcaecati excepturi? Quis eveniet qui minima, dolorum rem delectus, minus facere magnam, illum numquam laborum voluptas ab? Libero, odit soluta. Dolor, fugiat.
+            Quo, repellat. Architecto, natus, velit facilis, sequi sint corporis aliquid similique amet sed cum illo. Modi consequatur consequuntur inventore natus aliquam similique dolores totam delectus! Iste doloribus illo amet reiciendis?
+            Odit deleniti natus, dolor aspernatur autem assumenda ratione repudiandae, illum nam maiores optio tempore in veritatis dolorum quibusdam officia mollitia! Enim odio eligendi alias quia natus molestiae modi eum ex.
+            Maxime, illum incidunt? Recusandae autem aliquam repudiandae temporibus fugiat laboriosam in, quisquam eum dolores aperiam incidunt rem, laborum soluta, nostrum distinctio aut voluptatibus itaque quae doloremque eius iusto provident at.
+            Aspernatur fugiat totam nulla, perferendis doloribus vel dolore, expedita maxime nam culpa nemo vitae cupiditate in magnam, dolor provident illo iusto aliquam qui! Odit voluptatem magnam veniam, cumque eligendi impedit!
+            Saepe expedita perspiciatis corrupti voluptatem numquam animi fugiat sequi nemo libero omnis. Id consectetur, eum labore quia animi iure fuga porro accusantium iusto officiis sunt deserunt, exercitationem illum quaerat minima.
+            Ad eius nesciunt, velit dolorem beatae voluptates aliquid nobis reiciendis. Sint, officia. Incidunt nihil nulla magni assumenda sunt, reprehenderit alias! Minima delectus, illum quidem ipsa doloremque cupiditate eveniet dicta maxime!
+            Dolores iure amet aperiam debitis necessitatibus error dignissimos quas, enim modi praesentium sapiente illo omnis esse cupiditate, voluptate laboriosam exercitationem magnam quos aliquam repellendus quam possimus ut voluptatibus minima. Quasi?
+            Ratione facere maiores incidunt quo recusandae. Laborum consequatur fuga repudiandae quam possimus rem vitae facilis molestiae explicabo ducimus obcaecati ab architecto nobis suscipit delectus, sint itaque consectetur adipisci labore corrupti.
+            Esse laborum velit id totam quos unde eaque, debitis temporibus quisquam ut quis dolorum repellat illo ullam maiores deleniti qui, perspiciatis asperiores voluptatibus! Reprehenderit, repellendus alias praesentium voluptas animi eum?
+            Perspiciatis a dicta delectus obcaecati iusto explicabo nobis possimus, non totam cum officiis! Neque fugiat, veniam illo incidunt nisi exercitationem, quae, unde vitae architecto quibusdam officiis alias labore minima rerum?
+            Atque velit veritatis modi tempore fuga, consectetur quos ab sint soluta. Corporis ea error praesentium rerum. Ut soluta perspiciatis illo nesciunt, ad quas optio est sint. Velit minus consequuntur nihil.
+            Impedit id quidem ea deserunt maxime error non odit tenetur at rem molestiae aut excepturi nulla nemo accusantium, totam magnam nesciunt voluptatem. Eos obcaecati saepe tempore nemo. Dolor, dolore consequuntur.
+            Nam, minima. Consectetur nobis voluptates ut alias distinctio enim quod ducimus, consequatur, libero aperiam est voluptas maxime laboriosam repellendus? Quae voluptatem, mollitia temporibus accusantium accusamus quis deleniti eos cumque perferendis!
+            Architecto similique eveniet magni explicabo officiis numquam quia praesentium! Possimus, optio debitis itaque cupiditate enim obcaecati quam hic quod impedit dolor voluptate inventore perferendis ab earum culpa soluta voluptatem incidunt?
+            Esse, fugit quae aut debitis aspernatur molestias, impedit sint quia ad, tempora maxime eligendi id amet perferendis perspiciatis earum assumenda ducimus. Obcaecati mollitia voluptates quia reiciendis, magnam aliquam necessitatibus velit.
+            Consequuntur earum, ad quis, quam eos tempora provident aliquid voluptate recusandae atque nihil culpa. Aliquam eligendi, hic quisquam unde, consectetur maiores accusantium deserunt https://editor.method.ac/enim, provident accusantium, quam suscipit omnis deserunt saepe iure! Dolorem blanditiis amet adipisci cupiditate nihil autem tempora corporis optio. Quia!
+            Quam fugit adipisci qui et magnam eligendi veniam, delectus perferendis architecto eum eveniet itaque recusandae? Necessitatibus, consequatur cupiditate accusantium ipsum rem harum, porro obcaecati est officiis ut nesciunt amet sequi?
+          </p>
+        </div>
+        <div class="page2 my-5 mx-3">
+          <h2 class="text-center mb-2">2 Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, molestiae. Blanditiis beatae adipisci soluta similique incidunt doloremque in minima, sunt itaque ex aspernatur nesciunt aut expedita saepe, dignissimos animi laboriosam!
+            A saepe itaque dicta fugiat repellendus error laborum obcaecati excepturi? Quis eveniet qui minima, dolorum rem delectus, minus facere magnam, illum numquam laborum voluptas ab? Libero, odit soluta. Dolor, fugiat.
+            Quo, repellat. Architecto, natus, velit facilis, sequi sint corporis aliquid similique amet sed cum illo. Modi consequatur consequuntur inventore natus aliquam similique dolores totam delectus! Iste doloribus illo amet reiciendis?
+            Odit deleniti natus, dolor aspernatur autem assumenda ratione repudiandae, illum nam maiores optio tempore in veritatis dolorum quibusdam officia mollitia! Enim odio eligendi alias quia natus molestiae modi eum ex.
+            Maxime, illum incidunt? Recusandae autem aliquam repudiandae temporibus fugiat laboriosam in, quisquam eum dolores aperiam incidunt rem, laborum soluta, nostrum distinctio aut voluptatibus itaque quae doloremque eius iusto provident at.
+            Aspernatur fugiat totam nulla, perferendis doloribus vel dolore, expedita maxime nam culpa nemo vitae cupiditate in magnam, dolor provident illo iusto aliquam qui! Odit voluptatem magnam veniam, cumque eligendi impedit!
+            Saepe expedita perspiciatis corrupti voluptatem numquam animi fugiat sequi nemo libero omnis. Id consectetur, eum labore quia animi iure fuga porro accusantium iusto officiis sunt deserunt, exercitationem illum quaerat minima.
+            Ad eius nesciunt, velit dolorem beatae voluptates aliquid nobis reiciendis. Sint, officia. Incidunt nihil nulla magni assumenda sunt, reprehenderit alias! Minima delectus, illum quidem ipsa doloremque cupiditate eveniet dicta maxime!
+            Dolores iure amet aperiam debitis necessitatibus error dignissimos quas, enim modi praesentium sapiente illo omnis esse cupiditate, voluptate laboriosam exercitationem magnam quos aliquam repellendus quam possimus ut voluptatibus minima. Quasi?
+            Ratione facere maiores incidunt quo recusandae. Laborum consequatur fuga repudiandae quam possimus rem vitae facilis molestiae explicabo ducimus obcaecati ab architecto nobis suscipit delectus, sint itaque consectetur adipisci labore corrupti.
+            
+          </p>
+          <div class="row">
+            <div class="col-1"></div>
+            <div class="col-4 border-bottom border-dark">
+              <h4 class="text-center"><?= $responsable ?></h4>
+              <br>
+              <img src="privateFiles/firmas/Alejandro.png" alt="No está aqui" style="max-width: 100%">
+
+            </div>
+            <div class="col-2"></div>
+            <div class="col-4 border-bottom border-dark"><h4 class="text-center"><?= $contractor ?></h4></div>
+            <div class="col-1"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col"></div>
+  </div>
 </body>
 </html>
